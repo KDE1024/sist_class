537fa2043a25f4d3f966fc3c0d8d8707d341994b
KIM DAEUN <80617948+KDE1024@users.noreply.github.com>
2021-03-16 17:52:13 +0900

    예외 에러를 파일로 출력
    API를 보고 e.printStackTrace();에서 출력하는 내용을 Console이 아닌 파일 (c:/dev/err.log)로 출력해보세요.

--- Test.java
+++ Test.java
@@ -1,5 +1,9 @@
 package day0315;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintStream;
+
 public class Test {
 
 	public Test() throws ClassNotFoundException {
@@ -7,14 +11,19 @@ public class Test {
 	}//Test
 	
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException{
+		//API를 보고 e.printStackTrace();에서 출력하는 내용을 Console이 아닌 파일 (c:/dev/err.log)로 출력해보세요.
+		File log = new File("c:/dev/err.log");
+		PrintStream fos = null;
+		fos = new PrintStream(log);
+		System.setErr(fos);
 		try {
 			new Test();
 		} catch (ClassNotFoundException e) {
-			//API를 보고 e.printStackTrace();에서 출력하는 내용을 Console이 아닌 파일 (c:/dev/err.log)로 출력해보세요.
-			e.printStackTrace();
-			System.err.println("c:/dev/err.log");
-		}//end catch
+			e.printStackTrace(System.err);
+		}finally {
+			if(fos!=null) {fos.close();}//end if
+		}//finally
 	}//main
 
 }//class
